8f9ae227cddac530f223af1d6a7de15e0fdcd12a
Usuario <Usuario@CPD02-M01>
2024-04-16 11:36:38 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppLacoForEx1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLacoForEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 9; i++)
+            {
+
+                Console.Write(i);
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (i != j)
+                    {
+                        Console.Write(j);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
